a617c1cc60628ba6e497d40030634d8512e6d622
janbur <37516253+janbur@users.noreply.github.com>
2018-04-04 22:38:48 +0200

    Update and rename script_01-A_check_null_pcodes.py to script_02_QC_null_pcodes.py

---
+++ script_02_QC_null_pcodes.py
@@ -0,0 +1,73 @@
+# ###########################
+# NULL PCODES QC CHECK
+# ###########################
+
+import sys
+from qgis.core import *
+from PyQt4.QtCore import *
+from datetime import datetime, date, time
+
+print "############################"
+print "NULL PCODES QC CHECK"
+print "############################"
+startDate = datetime.utcnow()
+print "Started: " + str(startDate) + "\n"
+
+# input Pcode and Parent Pcode field names for all admin levels
+fnames = ["admin0Pcod","admin1Pcod","admin2Pcod","admin3Pcod"]
+fpnames = ["admin0Pcod","admin0Pcod","admin1Pcod","admin2Pcod"]
+
+# set layers
+lyrs = [layer for layer in qgis.utils.iface.legendInterface().layers() if layer.name() <> "locations_location"]
+
+nullids = []
+ftsaffected = []
+errorCount = 0
+qcstatus = ""
+
+
+# level counter
+l = 0
+
+# print input settings
+print "INPUT"
+print "Level\tLayer\tDateModif\tPcodeField\tPPcodeField\tCount"
+for lyr in lyrs:
+	print "{}\t{}\t{}\t{}\t{}\t{}".format(l,lyr.name(),datetime.fromtimestamp(os.path.getmtime(lyr.dataProvider().dataSourceUri().split("|")[0])),fnames[l],fpnames[l],lyr.featureCount())
+	l+=1
+l = 0
+
+print "\nDETAILS"
+for lyr in lyrs:
+	fts = lyr.getFeatures()
+	# check pcodes
+	tempnullids = []
+	for ft in fts:
+		ftn = str(ft[fnames[l]]).strip()
+		if ftn is 'NULL' or ftn =='':
+			tempnullids.append(ft.id())
+			ftsaffected.append(ft)
+			print "Level {} - feature {} has null Pcode".format(l,ft.id())
+			errorCount += 1
+	lyr.setSelectedFeatures(tempnullids)
+	nullids.append([lyr.name(), tempnullids])
+	l += 1
+	
+print "\nSUMMARY"
+print "\nNumber of features with empty pcodes:\t" + str(len(list(ftsaffected)))
+print "\nLayer\tCountFeaturesWithEmptyPcodes\tFeaturesIds"
+
+for i in nullids:
+	print i[0] + "\t" + str(len(list(i[1]))) + "\t" + str(list(i[1]))
+
+
+# update QC status
+if errorCount == 0:
+	qcstatus = "OK"
+else:
+	qcstatus = "MANUAL CHECK REQUIRED"
+
+endDate = datetime.utcnow()
+print "\nCompleted: " + str(endDate)
+print "Total processing time: " + str(endDate - startDate)
+print "\nQC STATUS:\t{}".format(qcstatus)
