b91d0960269570ea4822ade07c6a99b523004b11
janbur <jan.burdziej@gmail.com>
2018-04-17 13:48:19 +0200

    working version - null pcodes, dupl pcodes, null parent pcodes to be added: geom valid + ppcode valid

--- script_QC_etools_locations_PostGIS.py
+++ script_QC_etools_locations_PostGIS.py
@@ -7,6 +7,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtCore import QVariant
 from datetime import datetime, date, time
 import itertools
+import collections
 import os
 
 print "############################"
@@ -17,7 +18,7 @@ print "Started: {}\n".format(str(startDate))
 
 # input Pcode, Parent Pcode and Name fields for all admin levels
 id_field = "id"
-pc_field = "pcode"
+pc_field = "p_code"
 ppc_field = "parent_id"
 name_field = "name"
 gateway_ids = []
@@ -43,10 +44,13 @@ print "namepcode:\t{}".format(name_field)
 print "Threshold size for intersections: {}".format(thres)
 
 print "\nGateway ids"
-print "Fid\tId\tName"
+print "Level\tFid\tGateId\tGateName\tFtCount\tNullPcodes\tDuplPcodes\tNullPPcodes\tWrongPPcodes"
 cntryflag = 0
 errors = []
 results = []
+nullids = []
+pcodes = []
+ftsaffected = []
 errorCount = 0
 qcstatus = ""
 
@@ -60,91 +64,50 @@ def timediff():
 timediff.prevDate = datetime.utcnow()
 
 
-def geom_qc(l, fts):
-	print "\nAnalysing Level {}".format(l)
-	geomerrors = []
-	geomerrorcount = 0
-	# create a memory layer for intersections
-	mem_layer = QgsVectorLayer("MultiPolygon?crs=epsg:4326", "locations_overlaps", "memory")
-	mem_layer.startEditing()
-	pr = mem_layer.dataProvider()
-	pr.addAttributes(
-		[QgsField("lyrid", QVariant.String), QgsField("fid1", QVariant.Int), QgsField("fid2", QVariant.Int)])
+for g in gateway_tbl[0].getFeatures():
+	expr = QgsExpression("\"gateway_id\"={}".format(g["id"]))
+	for ft in loc_lyr[0].getFeatures(QgsFeatureRequest(expr)):
+		pcode = str(ft[pc_field]).strip()
+		if pcode is not 'NULL' and pcode != '':
+			pcodes.append(pcode)
+duplpcodes = ["'" + item + "'" for item, count in collections.Counter(pcodes).items() if count > 1]
+duplquery = ''
 
-	# prepare a loop on the input layer
-	polygons = []
-	for feature in fts:
-		print feature.id()
-		geom = feature.geometry()
-		if geom:
-			err = geom.validateGeometry()
-			if not err:
-				polygons.append(feature)
-			else:
-				polygons.append(feature)
-				geomerrors.append([err, feature.id()])
-				geomerrorcount += 1
-				for er in err:
-					print "\t{}".format(er.what())
-	#lyr.setSelectedFeatures([g[1] for g in geomerrors])
-	counter = 1
-	overlaps = []
-	combCount = len(list(itertools.combinations(polygons, 2)))
-	print combCount
-	for feature1, feature2 in itertools.combinations(polygons, 2):
-		if feature1.geometry().intersects(feature2.geometry()):
-			geom = feature1.geometry().intersection(feature2.geometry())
-			if geom.area() > thres:
-				print "\t {}% - {} is intersecting with {} (area: {}, {}%)".format(
-					str(round((float(counter) / combCount) * 100, 2)), feature1.id(), feature2.id(), geom.area(),
-					feature1.geometry().area() / geom.area() * 100)
-				feature = QgsFeature()
-				fields = mem_layer.pendingFields()
-				feature.setFields(fields, True)
-				feature.setAttributes([l, feature1.id(), feature2.id()])
-				feature.setGeometry(geom)
-				pr.addFeatures([feature])
-				mem_layer.updateExtents()
-				mem_layer.commitChanges
-				overlaps.append([feature1.id(), feature2.id()])
-				geomerrorcount += 1
-		counter += 1
-
-	mem_layer.commitChanges()
-	if len(list(overlaps)) > 0:
-		QgsMapLayerRegistry.instance().addMapLayer(mem_layer)
-	results.append([geomerrors, overlaps])
+for d in duplpcodes:
+	duplquery = ",".join(list(duplpcodes))
 
 
 for g in gateway_tbl[0].getFeatures():
 	expr = QgsExpression("\"gateway_id\"={}".format(g["id"]))
 	ftcount = 0
 	gateway_ids.append(g["id"])
-	geom_qc(l, loc_lyr[0].getFeatures(QgsFeatureRequest(expr)))
+
+	null_pcode_ids = []
+	null_ppcode_ids = []
+	loc_lyr[0].setSelectedFeatures(tempnullids)
+	nullids.append([loc_lyr[0].name(), tempnullids])
+
 	for ft in loc_lyr[0].getFeatures(QgsFeatureRequest(expr)):
+		pcode = str(ft[pc_field]).strip()
+		ppcode = str(ft[ppc_field]).strip()
+
+		if pcode is 'NULL' or pcode =='':
+			null_pcode_ids.append(ft.id())
+		if ppcode is 'NULL' or ppcode =='':
+			null_ppcode_ids.append(ft.id())
 		ftcount += 1
-	print "{}\t{}\t{}\t{}".format(g.id(), g['id'], g['name'], ftcount)
+
+	query = '"' + str(pc_field) + '" in (' + str(duplquery) + ')'
+	selection = loc_lyr[0].getFeatures(QgsFeatureRequest().setFilterExpression(query))
+	selids = [k.id() for k in selection]
+
+	print "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(l, g.id(), g['id'], g['name'], ftcount, len(list(null_pcode_ids)), len(list(selids)), len(list(null_ppcode_ids)))
 	if g["name"] == "Country":
 		cntryflag = 1
 	l += 1
 if cntryflag == 0:
 	errors.append("No Country level exists in gateway_id table!")
 
-print "\nGeometry QC Check Details"
-
-
-
-print "\nSummary"
-print "\nLevel\tGeomErrors\tIntersections"
-for r in results:
-	print "{}\t{}\t{}".format(l, len(r[0]), len(r[1]))
-	l += 1
-
-# update QC status
-if errorCount == 0:
-	qcstatus = "OK"
-else:
-	qcstatus = "MANUAL CHECK REQUIRED"
 
 endDate = datetime.utcnow()
 print "\nCompleted: " + str(endDate)
